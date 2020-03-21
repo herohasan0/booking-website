be16a17ee5f97df02fb699bc2497ee212766ee0e
kahasan <kahraman.h@outlook.com>
2020-03-22 00:57:02 +0300

    Pug yapısına geçildi.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
 const prefix = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
+const pug = require('gulp-pug');
 const reload = browserSync.reload;
 
 gulp.task('css', function() {
@@ -15,7 +15,7 @@ gulp.task('css', function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+gulp.task('browse', function() {
   // Serve files from the root of this project
   browserSync.init({
     notify: false,
@@ -24,6 +24,18 @@ gulp.task('default', function() {
     }
   });
 
-  gulp.watch('*.html').on('change', reload);
+  //gulp.watch('*.html').on('change', reload);
+  gulp.watch('./views/**/*.pug').on('change', reload);
+  //gulp.watch('views/**/*.pug', gulp.series('html'));
   gulp.watch('./scss/**/*.scss', gulp.series('css'));
 });
+
+gulp.task('html', function() {
+  return gulp
+    .src('./views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+    .on('end', reload);
+});
+
+gulp.task('default', gulp.series('browse', 'html', 'css'));
